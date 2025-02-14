1c4d8659a410336d80d57d879020490986e73c80
john-guioguio <190331326+john-guioguio@users.noreply.github.com>
2025-02-14 22:04:30 +0800

    app

--- src/lib/axiosClient.ts
+++ src/lib/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://test.openisland.ph",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://cms.openisland.ph",
   withCredentials: true, // ✅ Needed for CSRF and authentication
 });
 
